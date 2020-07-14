bdbc3aa46194fb9a49ce26d83319b467b2579abc
Dazacher <dazacher2000@yahoo.com>
2020-07-14 09:09:16 -0700

    Get rid of unused code in ResultsList.js file.

--- src/components/resultslist/ResultsList.js
+++ src/components/resultslist/ResultsList.js
@@ -43,35 +43,4 @@ const ResultsList = (props) => {
     );
 };
 
-// function ResultsList(props) {
-
-//     return (
-
-//         <form>
-//             <MDBDataTable striped bordered hover size="sm" responsive>
-//                 <thead>
-//                     <tr>
-//                         <th>Image</th>
-//                         <th sort="asc">First Name</th>
-//                         <th sort="asc">Last Name</th>
-//                         <th>Email</th>
-//                         <th>Phone Number</th>
-//                     </tr>
-//                 </thead>
-//                 <tbody>
-//                     {props.employeeRecords.map((person, id) => (
-//                         <tr key={id}>
-//                             <td sort><img alt={`${person.first} ${person.last}`} src={`./pictures/${person.picture}`}></img></td>
-//                             <td>{person.first}</td>
-//                             <td>{person.last}</td>
-//                             <td>{person.email}</td>
-//                             <td>{person.phone}</td>
-//                         </tr>
-//                     ))}
-//                 </tbody>
-//             </MDBDataTable>
-//         </form >
-//     )
-// };
-
 export default ResultsList;
